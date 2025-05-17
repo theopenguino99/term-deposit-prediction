6406b963f07e6adb4a24605af6723507765267df
Theo <qi-yang.er@student-cs.fr>
2025-05-17 14:14:46 +0800

    passed test_data_cleaner

---
+++ tests/test_data_cleaner.py
@@ -0,0 +1,56 @@
+import pytest
+import pandas as pd
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from data_loader import DataLoader
+from data_cleaner import DataCleaner
+
+def get_test_df():
+    loader = DataLoader()
+    loader.raw_data_path = "../data/bmarket.db"  # Change the raw data path as this script is not in the same directory as data_loader.py
+    return loader.load_data()
+
+def test_clean_data_real():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    cleaned = cleaner.clean_data(df)
+    assert isinstance(cleaned, pd.DataFrame)
+    assert not cleaned.empty
+
+def test_drop_columns():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.drop_columns(df.copy())
+    assert isinstance(df, pd.DataFrame)
+
+def test_handle_unknown_values():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.handle_unknown_values(df.copy())
+    assert isinstance(df, pd.DataFrame)
+
+def test_extract_age():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.extract_age(df.copy())
+    assert isinstance(df, pd.DataFrame)
+
+def test_impute():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.impute(df.copy())
+    assert isinstance(df, pd.DataFrame)
+
+def test_handle_negative_values():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.handle_negative_values(df.copy())
+    assert isinstance(df, pd.DataFrame)
+
+def test_remove_columns():
+    df = get_test_df()
+    cleaner = DataCleaner()
+    df = cleaner.remove_columns(df.copy())
+    assert isinstance(df, pd.DataFrame)
